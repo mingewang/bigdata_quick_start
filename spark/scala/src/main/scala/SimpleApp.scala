691432b926aa29dc75ee8c53f8782513e1f734e3
Min Wang <mingewang@gmail.com>
2023-12-03 18:21:26 -0600

    use hdfs sample

--- spark/scala/src/main/scala/SimpleApp.scala
+++ spark/scala/src/main/scala/SimpleApp.scala
@@ -2,9 +2,9 @@ import org.apache.spark.sql.SparkSession
 object SimpleApp {
   def main(args: Array[String]) {
     // Should be some file on your system
-    val logFile = "SimpleApp.scala" 
+    //val logFile = "src/main/scala/SimpleApp.scala" 
     // could be a file on hadoop hdfs
-    // val logFile = "hdfs://hadoop-master:9000/data/SimpleApp.scala" 
+    val logFile = "hdfs://hadoop-master:9000/data/SimpleApp.scala" 
     val spark = SparkSession.builder.appName("Simple Application").getOrCreate()
     // it is a dataset
     // you can use Spark-SQL or dataset API
